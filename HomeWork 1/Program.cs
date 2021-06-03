eb4a0f091b27b012f4bf8f7d4a0290ec23364e70
AnastasiiaKrestiuk <darina9220@gmail.com>
2021-06-03 18:05:52 +0300

    Добавьте файлы проекта.

---
+++ HomeWork 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Как Вас зовут?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сегодня " + DateTime.Now + );
+        }
+    }
+}
